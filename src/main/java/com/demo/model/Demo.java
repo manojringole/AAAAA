2f0dc33755b2676716e36052e3c75953b66a50c7
manojringole <manojringole@gmail.com>
2023-07-22 20:22:36 -0700

    Model package added A folder

---
+++ src/main/java/com/demo/model/Demo.java
@@ -0,0 +1,16 @@
+package com.demo.model;
+
+public class Demo {
+   
+	private int demoId;
+
+	public int getDemoId() {
+		return demoId;
+	}
+
+	public void setDemoId(int demoId) {
+		this.demoId = demoId;
+	}
+	
+	
+}
